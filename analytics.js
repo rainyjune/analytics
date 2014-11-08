599f3d8c26581f4d96a9e8c78eb6719e29d82835
rainyjune <rainyjune@live.cn>
2014-11-08 14:48:19 +0800

    Implement the getBrowser function

--- analytics.js
+++ analytics.js
@@ -82,12 +82,15 @@
    * @return {String} [unkown | Safari | Chrome | Android Browser | Opera | IE Mobile | IE | Firefox | UC]
    */
   function getBrowser() {
-    return "unknown";
+    var s = navigator.userAgent.toLowerCase();
+    var match = /(webkit)[ \/]([\w.]+)/.exec(s) ||
+        /(opera)(?:.*version)?[ \/]([\w.]+)/.exec(s) ||
+        /(msie) ([\w.]+)/.exec(s) ||
+        !/compatible/.test(s) && /(mozilla)(?:.*? rv:([\w.]+))?/.exec(s) ||
+        [];
+    return { name: match[1] || "", version: match[2] || "0" };
   }
   
-  /** 
-   * TODO
-   */
   function getHost() {
     return window.location.host;
   }
