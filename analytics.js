c65271dddc40e49bc9c497e0ce49306e8d8edfe3
Kang Chen <rainyjune@live.cn>
2015-04-22 17:12:51 +0800

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -30,43 +30,6 @@
     return false;
   }
   
-  // A String.trim() method for ECMAScript 3 
-  if(!String.prototype.trim) {
-    String.prototype.trim = function () {
-      return this.replace(/^\s+|\s+$/g,'');
-    };
-  }
-  // Production steps of ECMA-262, Edition 5, 15.4.4.14
-  // Reference: http://es5.github.io/#x15.4.4.14
-  if (!Array.prototype.indexOf) {
-    Array.prototype.indexOf = function(searchElement, fromIndex) {
-      var k;
-      if (this == null) {
-        throw new TypeError('"this" is null or not defined');
-      }
-      var O = Object(this);
-      var len = O.length >>> 0;
-      if (len === 0) {
-        return -1;
-      }
-      var n = +fromIndex || 0;
-      if (Math.abs(n) === Infinity) {
-        n = 0;
-      }
-      if (n >= len) {
-        return -1;
-      }
-      k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
-      while (k < len) {
-        if (k in O && O[k] === searchElement) {
-          return k;
-        }
-        k++;
-      }
-      return -1;
-    };
-  }
-  
   function pageTracking(params) {
     var args = urlArgs();
     var paramObj = {
