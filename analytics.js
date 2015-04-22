a08cb884b7465a3c19146535d10fd13f97285333
rainyjune <rainyjune@live.cn>
2015-04-22 17:22:33 +0800

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -90,7 +90,7 @@
   
   function getRequestUrl(paramObj, params) {
     // TODO
-    var actionName = params.act ? params.act : (params.event ? "event" : "general");
+    var actionName = (params && params.act) ? params.act : (params && params.event ? "event" : "general");
     var host = "http://tongji.leju.com/?site=gather&ctl=gather&act=" + actionName;
     var serializedPramString = getSerializedPrams(paramObj, params);
     return host + serializedPramString;
